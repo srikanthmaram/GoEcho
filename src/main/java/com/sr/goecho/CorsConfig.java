ca7ad48e049dae7ba00dfe1e7def480a7d967472
srikanthmaram <maramsrikanth08@gmail.com>
2024-07-08 02:25:09 +0530

    modified base URLs

--- src/main/java/com/sr/goecho/CorsConfig.java
+++ src/main/java/com/sr/goecho/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://goecho.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
