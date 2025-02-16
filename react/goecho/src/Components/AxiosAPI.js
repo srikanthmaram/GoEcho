98a1500406b93c328b3c992f350f03209bcbef7c
srikanthmaram <maramsrikanth08@gmail.com>
2025-02-17 00:57:45 +0530

    webservice URL is updated

--- react/goecho/src/Components/AxiosAPI.js
+++ react/goecho/src/Components/AxiosAPI.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '',
+  baseURL: 'https://goecho-webservice.onrender.com',
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
